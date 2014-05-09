4aa4dcff8e24de97e6f04c27e28efd64c9ee6386
FilipeFalcao <lipefbs@hotmail.com>
2014-05-09 09:38:35 -0300

    adcionei um frame ao programa, adcionei mais botoes, consegui tratar os botoes como vetores na janela. ass: pavao

--- ProjetoP1/main.c
+++ ProjetoP1/main.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <gtk/gtk.h>
 
 /*janela_descrição_do_locdata*/
@@ -26,47 +25,99 @@ static void janela_equipe (GtkWidget *wid, GtkWidget *win)
   gtk_widget_destroy (dialog);
 }
 
-int main (int argc, char *argv[])
+static void janela_descricao_do_gtk (GtkWidget *wid, GtkWidget *win)
 {
+  GtkWidget *dialog = NULL;
 
-  /*declarando os widgets que serão utilizados*/
-  GtkWidget *button = NULL;
-  GtkWidget *win = NULL;
-  GtkWidget *vbox = NULL;
+  dialog = gtk_message_dialog_new (GTK_WINDOW (win), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_CLOSE, " O GTK+ 2 é um toolkit multi-plataforma para a criação de interfaces gráficas.\nÉ licenciado sob a licença GNU LGPL, permitindo que software proprietários e livres o utilizem em sua construção.\nÉ software livre e integra o projeto GNU.\nFoi desenvolvido inicialmente para o GIMP, por isso foi batizado de GIMP toolkit, com abreviação GTK+.\nFoi desenvolvido originalmente por Peter Mattis, Spencer Kimball e Josh MacDonald2.\nO GTK+ é muito popular, sendo usado em um grande número de aplicações e no ambiente de desktop GNOME (que por sua vez também é muito popular).\n");
+  gtk_window_set_position (GTK_WINDOW (dialog), GTK_WIN_POS_CENTER);
+  gtk_dialog_run (GTK_DIALOG (dialog));
+  gtk_widget_destroy (dialog);
+}
 
-  /*iniciando o GTK*/
-  /*a função gtk_init (&argc, &argv) é essencial para o funcionamente de qualquer aplicação GTK*/
-  g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, (GLogFunc) gtk_false, NULL);
-  gtk_init (&argc, &argv);
-  g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, g_log_default_handler, NULL);
+static void janela_porque_o_gtk (GtkWidget *wid, GtkWidget *win)
+{
+  GtkWidget *dialog = NULL;
 
-  /*criando a janela*/
-  win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_container_set_border_width (GTK_CONTAINER (win), 100);
-  gtk_window_set_title (GTK_WINDOW (win), "LocData");
-  gtk_window_set_position (GTK_WINDOW (win), GTK_WIN_POS_CENTER);
-  gtk_widget_realize (win);
-  g_signal_connect (win, "destroy", gtk_main_quit, NULL);
+  dialog = gtk_message_dialog_new (GTK_WINDOW (win), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_CLOSE, "Além de se tratar de um software livre, o GTK é essencialmente uma interface de programação de aplicativos orientada a objetos.\nEmbora completamente escrita em C, é implementada usando a idéia de classes e funções callback (ponteiros para funções).\nImpressionante!\n");
+  gtk_window_set_position (GTK_WINDOW (dialog), GTK_WIN_POS_CENTER);
+  gtk_dialog_run (GTK_DIALOG (dialog));
+  gtk_widget_destroy (dialog);
+}
 
-  /*criando uma caixa vertical com botões*/
-  vbox = gtk_vbox_new (TRUE, 6);
-  gtk_container_add (GTK_CONTAINER (win), vbox);
+int main( int argc, char *argv[])
+{
+  GtkWidget *window;
+  GtkWidget *fixed;
+  GtkWidget *frame1;
 
-  /*criando os botões e atribuindo funções a eles*/
-  button = gtk_button_new_with_label ("Descição do LocData");
-  g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (janela_descricao_do_locdata), (gpointer) win);
-  gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
+  GtkWidget *button1;
+  GtkWidget *button2;
+  GtkWidget *button3;
+  GtkWidget *button4;
+  GtkWidget *button5;
 
-  button = gtk_button_new_with_label ("Equipe");
-  g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (janela_equipe), (gpointer) win);
-  gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
+  gtk_init(&argc, &argv);
 
-  button = gtk_button_new_from_stock (GTK_STOCK_CLOSE);
-  g_signal_connect (button, "clicked", gtk_main_quit, NULL);
-  gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title (GTK_WINDOW (window), "Projeto de P1");
 
-  /*iniciando o loop main() e esperando a ocorrência de um evento*/
-  gtk_widget_show_all (win);
-  gtk_main ();
+  /* Here we connect the "destroy" event to a signal handler */
+  g_signal_connect (window, "destroy",
+		    G_CALLBACK (gtk_main_quit), NULL);
+
+  gtk_widget_set_size_request (window, 450, 280);
+  /* Sets the border width of the window. */
+  gtk_container_set_border_width (GTK_CONTAINER (window), 10);
+  g_signal_connect (window, "destroy", gtk_main_quit, NULL);
+
+  frame1 = gtk_frame_new(NULL);
+  gtk_container_add (GTK_CONTAINER (window), frame1);
+
+  /* Set the frame's label */
+  gtk_frame_set_label (GTK_FRAME (frame1), "Primeira apresentação do projeto de P1\nAqui serão apresentadas informações sobre o LocData e o GTK\n\nO LocData:                                                         O GTK:");
+
+  /* Align the label at the right of the frame */
+  gtk_frame_set_label_align (GTK_FRAME (frame1), 0.0, 0.0);
+
+  /* Set the style of the frame */
+  gtk_frame_set_shadow_type (GTK_FRAME (frame1), GTK_SHADOW_ETCHED_OUT);
+
+
+  fixed = gtk_fixed_new();
+  gtk_container_add(GTK_CONTAINER(frame1), fixed);
+
+  button1 = gtk_button_new_with_label("Sobre o LocData");
+  g_signal_connect (G_OBJECT (button1), "clicked", G_CALLBACK (janela_descricao_do_locdata), (gpointer) window);
+  gtk_fixed_put(GTK_FIXED(fixed), button1, 10, 30);
+  gtk_widget_set_size_request(button1, 150, 35);
+
+  button2 = gtk_button_new_with_label("Equipe desenvolvedora");
+  g_signal_connect (G_OBJECT (button2), "clicked", G_CALLBACK (janela_equipe), (gpointer) window);
+  gtk_fixed_put(GTK_FIXED(fixed), button2, 10, 80);
+  gtk_widget_set_size_request(button2, 200, 35);
+
+  button3 = gtk_button_new_with_label("Sobre o GTK");
+  g_signal_connect (G_OBJECT (button3), "clicked", G_CALLBACK (janela_descricao_do_gtk), (gpointer) window);
+  gtk_fixed_put(GTK_FIXED(fixed), button3, 260, 30);
+  gtk_widget_set_size_request(button3, 120, 35);
+
+  button4 = gtk_button_new_with_label("Porque o GTK?");
+  g_signal_connect (G_OBJECT (button4), "clicked", G_CALLBACK (janela_porque_o_gtk), (gpointer) window);
+  gtk_fixed_put(GTK_FIXED(fixed), button4, 260, 80);
+  gtk_widget_set_size_request(button4, 135, 35);
+
+  button5 = gtk_button_new_with_label("Sair");
+  g_signal_connect (button5, "clicked", gtk_main_quit, NULL);
+  gtk_fixed_put(GTK_FIXED(fixed), button5, 140, 140);
+  gtk_widget_set_size_request(button5, 135, 35);
+
+  g_signal_connect_swapped(G_OBJECT(window), "destroy",
+      G_CALLBACK(gtk_main_quit), NULL);
+
+  gtk_widget_show_all(window);
+
+  gtk_main();
+
   return 0;
 }
