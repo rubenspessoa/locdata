174d2baebba33dd5dea9961b95f7a9f43c4c96fe
FilipeFalcao <lipefbs@hotmail.com>
2014-05-22 17:39:09 -0300

    tela inicial refeita

--- ProjetoP1/main.c
+++ ProjetoP1/main.c
@@ -28,37 +28,27 @@ int main (int argc, char *argv [])
     g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
     g_signal_connect_swapped (window, "delete-event", G_CALLBACK (gtk_widget_destroy), window);
 
-    /*criando caixa vertical*/
-    vbox = gtk_vbox_new (FALSE, 0);
-    gtk_container_add (GTK_CONTAINER (window), vbox);
-
-    /*inserindo imagem: logo do LocData*/
-    image = gtk_image_new_from_file ("locdatalogonobg130x130.png");
-    gtk_box_pack_start (GTK_BOX (vbox), image, TRUE, TRUE, 0);
-
-    /*inserindo imagem: nome colorido do LocData*/
-    image2 = gtk_image_new_from_file("locdataname.png");
-    gtk_box_pack_start (GTK_BOX (vbox), image2, TRUE, TRUE, 0);
-
-    /*criando widget fixed*/
     fixed = gtk_fixed_new ();
-    gtk_container_add (GTK_CONTAINER(vbox), fixed);
+    gtk_container_add (GTK_CONTAINER(window), fixed);
+
+    image = gtk_image_new_from_file ("ldlogo4.png");
+    gtk_fixed_put (GTK_FIXED (fixed), image, 288, 70);
 
     /*criando botão "registre-se"*/
     button = gtk_button_new_with_label ("Registre-se");
     g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (janela_cadastro_de_empresas), (gpointer) window);
-    gtk_fixed_put (GTK_FIXED (fixed), button, 290, 50);
+    gtk_fixed_put (GTK_FIXED (fixed), button, 290, 300);
     gtk_widget_set_size_request (button, 200, 35);
 
     /*criando botão "já sou registrado"*/
     button = gtk_button_new_with_label ("Já sou registrado");
     g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (janela_login), (gpointer) window);
-    gtk_fixed_put (GTK_FIXED (fixed), button, 290, 100);
+    gtk_fixed_put (GTK_FIXED (fixed), button, 290, 350);
     gtk_widget_set_size_request (button, 200, 35);
 
     button = gtk_button_new_with_label ("Sobre o LocData");
     g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (sobre_o_locdata), (gpointer) window);
-    gtk_fixed_put (GTK_FIXED (fixed), button, 290, 150);
+    gtk_fixed_put (GTK_FIXED (fixed), button, 290, 400);
     gtk_widget_set_size_request (button, 200, 35);
 
     /*mostrando todos os widgets dentro de janela*/
