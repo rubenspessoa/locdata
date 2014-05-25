0620c96aa7bd39ba6f3819bb683c7a9d8762e11a
FilipeFalcao <lipefbs@hotmail.com>
2014-05-24 22:45:45 -0300

    adcionei a mudança no mecanismo de visualizaçao de senhas para o login também

--- ProjetoP1/gtkLocData.c
+++ ProjetoP1/gtkLocData.c
@@ -811,10 +811,12 @@ void janela_login ()
 {
     GtkWidget *window;
     GtkWidget *vbox;
+    GtkWidget *hbox;
     GtkWidget *fixed;
     GtkWidget *button;
     GtkWidget *label;
     GtkWidget *entry1, *entry2;
+    GtkWidget *check;
     gint tmp_pos;
 
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
@@ -846,11 +848,23 @@ void janela_login ()
 
     entry2 = gtk_entry_new ();
     gtk_entry_set_max_length (GTK_ENTRY (entry2), 50);
+    gtk_entry_set_visibility (entry2, FALSE);
     g_signal_connect (entry2, "changed", G_CALLBACK (enter_callback5), entry2);
     tmp_pos = GTK_ENTRY (entry2)->text_length;
     gtk_editable_select_region (GTK_EDITABLE (entry2), 0, GTK_ENTRY (entry2)->text_length);
     gtk_box_pack_start (GTK_BOX (vbox), entry2, TRUE, TRUE, 0);
     gtk_widget_show (entry2);
+
+    hbox = gtk_hbox_new (FALSE, 0);
+    gtk_container_add (GTK_CONTAINER (vbox), hbox);
+    gtk_widget_show (hbox);
+
+    /*criando um botão "toogle" de seleção*/
+    check = gtk_check_button_new_with_label ("Visualizar senha");
+    gtk_box_pack_start (GTK_BOX (hbox), check, TRUE, TRUE, 0);
+    g_signal_connect (check, "toggled", G_CALLBACK (entry_toggle_visibility), entry2);
+    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (check), FALSE);
+    gtk_widget_show (check);
 
     fixed = gtk_fixed_new ();
     gtk_container_add (GTK_CONTAINER (vbox), fixed);
